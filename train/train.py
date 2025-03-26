c67e8738a609f1624312a8919b932697b4774b85
liusf15 <sliu@flatironinstitute.org>
2025-03-26 16:16:18 -0400

    update train

--- train/train.py
+++ train/train.py
@@ -1,6 +1,9 @@
-import numpy as np
 import jax
+import jax.numpy as jnp
 import optax
+
+from flows.realnvp import RealNVP
+from flows.one_dim_flow import OneDSplineFlow
 
 def train(model, params, samples, contexts, learning_rate=0.01, max_iter=500):
     optimizer = optax.adam(learning_rate)
@@ -23,3 +26,17 @@ def train(model, params, samples, contexts, learning_rate=0.01, max_iter=500):
     params, opt_state = carry
     losses = list(losses)
     return params, losses
+
+
+def train_nf(samples, contexts, learning_rate, max_iter, hidden_dims=[32, 32], n_layers=8, num_bins=20):
+    d = samples.shape[1]
+    if d == 1:
+        model = OneDSplineFlow(context_dim=1, hidden_dims=hidden_dims, num_bins=num_bins)
+        params = model.init(jax.random.key(0), jnp.ones((1, )), context=jnp.ones((1, 1)))
+        samples = samples.flatten()
+    else:
+        model = RealNVP(dim=d, n_layers=n_layers, hidden_dims=hidden_dims)
+        params = model.init(jax.random.key(0), jnp.ones((1, d)), context=jnp.ones((1, d)))
+
+    params, losses = train(model, params, samples, contexts, learning_rate=learning_rate, max_iter=max_iter)
+    return model, params, losses
